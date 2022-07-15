ce7134f1bb660af90385af389e86e894cc3c131d
MMargelli <102836896+MMargelli@users.noreply.github.com>
2022-07-15 20:28:01 -0300

    Add files via upload

---
+++ AutomacaoE2E/ProjetoAutomacao/src/test/java/runner/Executa.java
@@ -0,0 +1,32 @@
+package runner;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Executa {
+
+	
+	
+	@Given("que eu esteja no site da shoestock")
+	public void que_eu_esteja_no_site_da_shoestock() {
+		
+     
+	}
+
+	@When("escolher o produto")
+	public void escolher_o_produto() {
+
+	}
+
+	@When("clicar em comprar")
+	public void clicar_em_comprar() {
+
+	}
+
+	@Then("valido o resumo da compra")
+	public void valido_o_resumo_da_compra() {
+
+	}
+
+}
